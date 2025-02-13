4f899d06a0cf27f9fec511e21d8a2ebe715f3b8f
MIFDAL <saidilmifdal15@gmail.com>
2025-02-13 10:48:01 +0800

    add admin dashboard layout and routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
